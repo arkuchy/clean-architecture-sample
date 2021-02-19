1ad57c7d2f855f75f82856e028c89ba3f684362f
ari1021 <o.pe.3.vndt.ku@gmail.com>
2021-02-19 21:34:47 +0900

    chore: 初期データの挿入ファイルを追加

---
+++ db/init/2_dml.sql
@@ -0,0 +1,14 @@
+use clean_architecture;
+
+SET NAMES utf8mb4;
+
+INSERT INTO `user` (`id`,`name`) VALUES ("1","name1");
+INSERT INTO `user` (`id`,`name`) VALUES ("2","name2");
+INSERT INTO `user` (`id`,`name`) VALUES ("3","name3");
+INSERT INTO `user` (`id`,`name`) VALUES ("4","name4");
+INSERT INTO `user` (`id`,`name`) VALUES ("5","name5");
+INSERT INTO `user` (`id`,`name`) VALUES ("6","name6");
+INSERT INTO `user` (`id`,`name`) VALUES ("7","name7");
+INSERT INTO `user` (`id`,`name`) VALUES ("8","name8");
+INSERT INTO `user` (`id`,`name`) VALUES ("9","name9");
+INSERT INTO `user` (`id`,`name`) VALUES ("10","name10");
